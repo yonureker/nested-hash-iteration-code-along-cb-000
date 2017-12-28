c6679f48ee7d96e26f678eb3612cbff68dc91ae1
Onur Eker <yonureker@gmail.com>
2017-12-28 05:28:17 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -23,11 +23,11 @@ end
 
 
 def remove_strawberry(contacts)
-    contacts.each do |person, contact_details_hash|
-      contact_details_hash.each do |attribute, data|
-        if attribute == :favorite_ice_cream_flavors
-          data.delete_if {|ice_cream| ice_cream == "strawberry"}
-        end
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
     end
   end
+end
